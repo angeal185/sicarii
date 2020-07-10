5fe4ed0d8d1d71a5162ce723070b8aaa58909f1b
ben <beneaves01@hotmail.com>
2020-07-10 17:01:40 +0800

    gh-pages init

--- main.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  server: require('./lib/server/worker').server,
-  router: require('./lib/server/worker').router,
-  crypt: require('./lib/utils/crypt')
-}
--- master.js
+++
@@ -1,8 +0,0 @@
-const config = require(process.env.config_file),
-Logs = require('./lib/utils/logs'),
-logs = new Logs(config.logs, config.compression),
-{ server, Cache } = require('./lib/server/master'),
-Sync = require('./lib/extends/sync'),
-sync = new Sync(server, logs);
-
-module.exports = { Cache, server, logs, sync};
